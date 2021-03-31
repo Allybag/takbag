093c1fbd2df3f97e48a24024f6e892b6619cff64
Ally <bagkingally@gmail.com>
2021-03-31 23:51:02 +0100

    Square::mTopStone is now included in Square::mStack Untested implementations of Square::give and Square::take

--- Square.h
+++ Square.h
@@ -5,13 +5,17 @@
 #include <string>
 #include <sstream>
 
+// Representation:
+// mTopStone describes the surface stone and mStack[0]
+// When we add a stone of colour c, we set mStack[mCount] = c, increment mCount and set mTopStone accordingly
+// When we remove n stones, we subtract n from mCount, and set mTopStone = mStack[mCount - 1]
 struct Square
 {
-    Stone topStone;
-    uint8_t reserveCount;
-    uint32_t stack; // A reserveCount long bitSet, 0 is white and 1 is black
+    Stone mTopStone;
+    uint8_t mCount;
+    uint32_t mStack; // A mCount long bitSet, 0 is white and 1 is black
 
-    Square() : topStone(Stone::Blank), reserveCount(0), stack(0) { }
+    Square() : mTopStone(Stone::Blank), mCount(0), mStack(0) { }
     Square(const Square&) noexcept = default;
     Square(Square&&) noexcept = default;
     Square& operator=(const Square&) noexcept = default;
@@ -20,17 +24,20 @@ struct Square
 
     std::size_t count() const;
     std::string print() const;
+
+    void give(Stone stone, uint8_t count, uint8_t allStones);
+    void take(std::size_t count);
 };
 
 std::size_t Square::count() const
 {
-    return topStone == Stone::Blank ? 0 : reserveCount + 1;
+    return mCount;
 }
 
 std::string Square::print() const
 {
     std::stringstream output;
-    switch (topStone)
+    switch (mTopStone)
     {
         case Stone::Blank:
             return " ";
@@ -54,8 +61,37 @@ std::string Square::print() const
             break;
     }
 
-    for (std::size_t i = 0; i < reserveCount; ++i)
-        output << (stack & (1 >> i) ? "f" : "F");
+    // TODO: Test this bit twiddling
+    for (std::size_t i = 1; i < mCount; ++i) // Start at 1 as we've already done mTopStone
+        output << (mStack & (1 << i) ? "f" : "F");
 
     return output.str();
+}
+
+void Square::give(Stone topStone, uint8_t count, uint8_t allStones)
+{
+    assert(!(mTopStone & StoneBits::Standing));
+    assert(count);
+    assert(mCount + count < 32); // Could have more than 32 stones in a stack
+
+    mTopStone = topStone;
+    for (std::size_t i = 0; i < count; ++i)
+    {
+        int currentStone = allStones & (1 << i);
+        // Apparently works for 2's complement representation, now guaranteed in c++
+        // Sets the mCount bit of mStack to currentStone
+        // TODO: Test this bit twiddling
+        mStack ^= (-currentStone ^ mStack) & (1 << mCount);
+        mCount++;
+    }
+}
+
+void Square::take(std::size_t count)
+{
+    assert(mTopStone != Stone::Blank);
+    assert(mCount >= count);
+
+    // We let mStack contain old garbage beyone mCount as we shouldn't use it
+    mCount -= count;
+    mTopStone = (mCount == 0 ? Stone::Blank : (mStack & 1 << mCount) ? Stone::BlackFlat : Stone::WhiteFlat);
 }
