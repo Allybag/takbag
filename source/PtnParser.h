907bbd61fcb515bfed621c15cd6ce21e513289cf
Ally <bagkingally@gmail.com>
2021-04-10 11:04:04 +0100

    Create Parser, which transforms a vector of tokens into a vector of Nodes

--- source/PtnParser.h
+++ source/PtnParser.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <cstddef>
-#include "ctre.hpp" // https://github.com/hanickadot/compile-time-regular-expressions
+#include <utility>
+#include <unordered_map>
+
 #include "Ptn.h"
+#include "Lexer.h"
 
 // The structure and design of this Parser is taken from the great free tutorial at
 // https://www.destroyallsoftware.com/screencasts/catalog/a-compiler-from-scratch
@@ -10,57 +13,229 @@
 // The PTN Spec followed is here: https://ustak.org/portable-tak-notation/
 // With additional notes on tags taken from here: https://www.reddit.com/r/Tak/wiki/ptn_file_format
 
-enum class TokenTypes : uint8_t
+enum class PtnTag
 {
-    TagOpen, // [
-    TagClose, // ]
-    TagKey, // Site (alphanum_)
-    TagData, // "PlayTak.com" (".+")
-    PlyNum, // 1. (num+.)
-    Ply, // 5a2>1121*''!! (alphanum+-<>*'?!) in various orders
-    GameResult, // R-O (R-0F1) in various orders
-    CommentOpen, // {
-    CommentClose, // }
-    Comment, // Anything is valid in a comment
+    // Required Tags
+    Player1,
+    Player2,
+    Size,
+    Date,
+    Result,
+
+    // Optional Tags
+    Clock,
+    Komi,
+    Flats,
+    Caps,
+    Time,
+    Site,
+    Event,
+    Round
 };
 
-
-#if 0
-[Site "PlayTak.com"]
-[Event "Online Play"]
-[Date "2018.10.28"]
-[Time "16:10:44"]
-[Player1 "NohatCoder"]
-[Player2 "fwwwwibib"]
-[Clock "10:0 +20"]
-[Result "R-0"]
-[Size "6"]
-
-1. a6 f6
-2. d4 c4
-3. d3 c3
-4. d5 c5
-5. d2 Ce4
-6. c2 e3
-7. e2 b2
-8. Cb3 1e4<1
-9. 1d3<1 Sd1
-10. a3 1d1+1
-#endif
-
-
-struct Lexer
+struct PtnGame
 {
-    // Tokens: [, ],
+    std::vector<PtnTurn> mMoves;
+    std::unordered_map<PtnTag, std::string> mTags;
+    std::unordered_map<std::string, std::string> mUnknownTags;
 
+    std::size_t mSize;
+
+    PtnGame(std::stringstream& ptnStream); // A stream of lines from a PTN File
 };
 
-struct Parser
+enum class NodeType : uint8_t
 {
-
+    TagNode,
+    PlaceNode,
+    MoveNode,
+    ResultNode,
+    CommentNode,
 };
 
-struct Generator
+std::ostream& operator<<(std::ostream& stream, NodeType nodeType)
 {
+    switch (nodeType)
+    {
+        case NodeType::TagNode:
+            stream << "Tag";
+            break;
+        case NodeType::PlaceNode:
+            stream << "Place";
+            break;
+        case NodeType::MoveNode:
+            stream << "Move";
+            break;
+        case NodeType::ResultNode:
+            stream << "Result";
+            break;
+        case NodeType::CommentNode:
+            stream << "Comment";
+            break;
+    }
+    return stream;
+}
 
+struct Node
+{
+    Node(NodeType type, std::string string) : mType(type), mString(std::move(string)) { }
+
+    NodeType mType;
+    std::string mString;
 };
+
+std::ostream& operator<<(std::ostream& stream, const Node& node)
+{
+    stream << "Node of type " << node.mType << " with string " << node.mString;
+    return stream;
+}
+
+class Parser
+{
+    void parseTag(const std::vector<Token>& tokens);
+    void parseResult(const std::vector<Token>& tokens);
+    void parseComment(const std::vector<Token>& tokens);
+    void parsePly(const std::vector<Token>& tokens);
+
+
+    // std::vector<PtnGame> mGames;
+    std::vector<Token> mRemainingTokens;
+    std::vector<Node> mParsedNodes;
+    std::size_t mIndex;
+
+public:
+    void accept(const std::vector<Token>& tokens);
+    void flush();
+    const std::vector<Node>& getNodes() { return mParsedNodes; }
+};
+
+void Parser::parseTag(const std::vector<Token>& tokens)
+{
+    if (!mRemainingTokens.empty())
+        flush();
+
+    assert(tokens[mIndex].mType == TokenType::TagOpen);
+    assert(tokens[mIndex + 1].mType == TokenType::TagKey);
+    assert(tokens[mIndex + 2].mType == TokenType::TagData);
+    assert(tokens[mIndex + 3].mType == TokenType::TagClose);
+    std::string tagKey = tokens[mIndex + 1].mValue;
+    tagKey.append(":").append(tokens[mIndex + 2].mValue);
+    mParsedNodes.emplace_back(NodeType::TagNode, tagKey);
+    mIndex += 4;
+}
+
+void Parser::parseResult(const std::vector<Token>& tokens)
+{
+    assert(tokens[mIndex].mType == TokenType::GameResult);
+    mParsedNodes.emplace_back(NodeType::ResultNode, tokens[mIndex].mValue);
+    ++mIndex;
+}
+
+void Parser::parseComment(const std::vector<Token> &tokens)
+{
+    assert(tokens[mIndex].mType == TokenType::Comment);
+    mParsedNodes.emplace_back(NodeType::ResultNode, tokens[mIndex].mValue);
+    ++mIndex;
+}
+
+void Parser::parsePly(const std::vector<Token>& tokens)
+{
+    // PlyNum, 0 or more comments, PlaceMove or MoveMove, 0 or more comments, PlaceMove or MoveMove or GameResult (or nothing)
+    std::size_t originalIndex = mIndex;
+    assert(tokens[mIndex].mType == TokenType::PlyNum);
+    int moveNum = std::stoi(tokens[mIndex].mValue);
+    ++mIndex;
+    int movesRemaining = 2;
+    std::vector<Node> nodes;
+    while (mIndex != tokens.size() && movesRemaining)
+    {
+        switch (tokens[mIndex].mType)
+        {
+            case TokenType::PlyNum:
+            {
+                int nextMoveNum = std::stoi(tokens[mIndex].mValue);
+                assert(nextMoveNum == 1 || nextMoveNum == moveNum + 1);
+                assert(movesRemaining == 1);
+                movesRemaining = 0;
+                break;
+            }
+            case TokenType::TagOpen:
+            case TokenType::GameResult:
+            case TokenType::End:
+                assert(movesRemaining == 1);
+                movesRemaining = 0;
+                break;
+            case TokenType::PlaceMove:
+                nodes.emplace_back(NodeType::PlaceNode, tokens[mIndex].mValue);
+                movesRemaining -= 1;
+                mIndex++;
+                break;
+            case TokenType::MoveMove:
+                nodes.emplace_back(NodeType::MoveNode, tokens[mIndex].mValue);
+                movesRemaining -= 1;
+                mIndex++;
+                break;
+            case TokenType::Comment:
+                parseComment(tokens);
+                break;
+            case TokenType::TagClose:
+            case TokenType::TagKey:
+            case TokenType::TagData:
+                assert(false);
+                break;
+        }
+    }
+
+    if (movesRemaining)
+    {
+        for (std::size_t index = originalIndex; index != mIndex; ++index)
+            mRemainingTokens.push_back(tokens[index]);
+    }
+    else
+    {
+        mParsedNodes.insert(mParsedNodes.end(), nodes.begin(), nodes.end());
+    }
+}
+
+void Parser::accept(const std::vector<Token>& tokens)
+{
+    mIndex = 0;
+    while (mIndex != tokens.size())
+    {
+        switch (tokens[mIndex].mType)
+        {
+            case TokenType::TagOpen:
+                parseTag(tokens);
+                break;
+            case TokenType::PlyNum:
+                parsePly(tokens);
+                break;
+            case TokenType::GameResult:
+                parseResult(tokens);
+                break;
+            case TokenType::Comment:
+                parseComment(tokens);
+                break;
+            case TokenType::TagClose:
+            case TokenType::TagData:
+            case TokenType::TagKey:
+            case TokenType::PlaceMove:
+            case TokenType::MoveMove:
+            case TokenType::End:
+                assert(false); // None of these tokens are valid to begin a Node
+                break;
+        }
+    }
+}
+
+void Parser::flush()
+{
+    if (mRemainingTokens.empty())
+        return;
+
+    mIndex = 0;
+    mRemainingTokens.push_back(Token{TokenType::End, ""});
+    parsePly(mRemainingTokens);
+    mIndex = 0;
+    mRemainingTokens.clear();
+}
--- test/testPtnParser.cpp
+++ test/testPtnParser.cpp
@@ -6,24 +6,23 @@
 #include <fstream>
 
 #include "Lexer.h"
+#include "PtnParser.h"
 
 int main()
 {
     Lexer lexer;
-    std::ifstream fileStream("/Users/ally/clones/takbag/games/LongStackyGame.ptn");
+    Parser parser;
+    std::ifstream fileStream("/Users/ally/clones/takbag/games/ShortDump.ptn");
 
     std::string line;
-    std::vector<Token> tokens;
     while (getline(fileStream, line))
     {
         auto items = lexer.tokenise(line);
-        for (const auto& item : items)
-        {
-            tokens.push_back(item);
-        }
+        parser.accept(items);
     }
+    parser.flush();
     std::cout << "Tokens: \n";
-    for (const auto& token : tokens)
-        std::cout << token << std::endl;
+    for (const auto& node : parser.getNodes())
+        std::cout << node << std::endl;
 
 }
