4369a047b41650c00c66c2cc30c1d17219419802
Ally <bagkingally@gmail.com>
2021-04-19 17:12:03 +0100

    Add a chooseMoveRandom function for engine to choose random moves rather than the first move it generates.

--- source/engine/Engine.cpp
+++ source/engine/Engine.cpp
@@ -1,10 +1,37 @@
 #include "Engine.h"
 #include "tak/Position.h"
 
+#include <random>
+
+template <typename Container>
+auto chooseRandomElement(const Container& container) -> typename Container::const_iterator
+{
+    static std::random_device gRandomDevice;
+    static std::default_random_engine gRandomEngine{ gRandomDevice() };
+    std::uniform_int_distribution<std::size_t> range(0, container.size() - 1);
+    auto element = container.begin();
+    auto elementNum = range(gRandomEngine);
+    std::advance(element, elementNum);
+    return element;
+}
+
 std::string Engine::chooseMove(const Position& position)
+{
+    return chooseMoveRandom(position);
+}
+
+std::string Engine::chooseMoveFirst(const Position& position)
 {
     assert(position.checkResult() == Result::None);
     auto moves = position.generateMoves();
     auto move = moves.front();
     return moveToPtn(move, position.size());
+}
+
+std::string Engine::chooseMoveRandom(const Position& position)
+{
+    assert(position.checkResult() == Result::None);
+    auto moves = position.generateMoves();
+    auto randomMove = chooseRandomElement(moves);
+    return moveToPtn(*randomMove, position.size());
 }
--- source/engine/Engine.h
+++ source/engine/Engine.h
@@ -6,6 +6,8 @@ class Position;
 
 class Engine
 {
+    std::string chooseMoveFirst(const Position& position);
+    std::string chooseMoveRandom(const Position& position);
 public:
     std::string chooseMove(const Position& position);
 };
