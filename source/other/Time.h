709276dbd5f8afbdd48f501b2496967f758d36fe
Ally <bagkingally@gmail.com>
2021-05-05 19:32:10 +0100

    Add other/Time.h as I'm finding chrono a chore to use, returns current time in mics since epoch

---
+++ source/other/Time.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+#include <sys/time.h>
+
+static constexpr int64_t micsInSecond = 1000000LL;
+
+int64_t timeInMics()
+{
+    struct timeval currentTime;
+    gettimeofday(&currentTime, nullptr);
+    return currentTime.tv_sec * micsInSecond + currentTime.tv_usec;
+}
