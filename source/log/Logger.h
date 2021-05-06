f322f326a5f40186e7e55ac85815646f62d5e9e9
Ally <bagkingally@gmail.com>
2021-05-06 17:27:28 +0100

    Move Logger::operator<< definition outside class as GCC doesn't allow template specialisations within the class

--- source/log/Logger.h
+++ source/log/Logger.h
@@ -30,34 +30,37 @@ public:
     Logger(std::string name, LogLevel logLevel) : mName(std::move(name)), mLogLevel(logLevel), mActiveLevel(LogLevel::Unset) { }
 
     template <typename InputT>
-    Logger& operator<<(InputT input)
-    {
-        assert(mActiveLevel != LogLevel::Unset);
-        if (shouldLog())
-            mActiveStream << input;
-        return *this;
-    }
-
-    template <>
-    Logger& operator<<(LogLevel logLevel)
-    {
-        assert(mActiveLevel == LogLevel::Unset);
-        mActiveLevel = logLevel;
-        return *this;
-    }
-
-    template <>
-    Logger& operator<<(const WriteMessage flushMessage)
-    {
-        assert(mActiveLevel != LogLevel::Unset);
-
-        // rootLogger is declared in RootLogger.h and defined in RootLogger.cpp
-        if (shouldLog())
-            rootLogger.log(mActiveLevel, mActiveStream.str(), flushMessage.mFuncName, mName);
-
-        std::stringstream emptyStream;
-        std::swap(mActiveStream, emptyStream);
-        mActiveLevel = LogLevel::Unset;
-        return *this;
-    }
+    inline Logger& operator<<(InputT input);
 };
+
+template <typename InputT>
+inline Logger& Logger::operator<<(InputT input)
+{
+	assert(mActiveLevel != LogLevel::Unset);
+	if (shouldLog())
+		mActiveStream << input;
+	return *this;
+}
+
+template <>
+inline Logger& Logger::operator<<(LogLevel logLevel)
+{
+	assert(mActiveLevel == LogLevel::Unset);
+	mActiveLevel = logLevel;
+	return *this;
+}
+
+template <>
+inline Logger& Logger::operator<<(const WriteMessage flushMessage)
+{
+	assert(mActiveLevel != LogLevel::Unset);
+
+	// rootLogger is declared in RootLogger.h and defined in RootLogger.cpp
+	if (shouldLog())
+		rootLogger.log(mActiveLevel, mActiveStream.str(), flushMessage.mFuncName, mName);
+
+	std::stringstream emptyStream;
+	std::swap(mActiveStream, emptyStream);
+	mActiveLevel = LogLevel::Unset;
+	return *this;
+}
