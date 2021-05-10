1b2ae66374f5b27496e185dff8dfb5abe9b2266e
Ally <bagkingally@gmail.com>
2021-05-10 21:30:10 +0100

    Add other/ArgParse.h, a simple function to parse command line arguments into a map of strings

---
+++ source/other/ArgParse.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "other/StringOps.h"
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cstdint>
+
+using OptionMap = std::unordered_map<std::string, std::string>;
+
+void flush(OptionMap& options, std::string& optionName, std::vector<std::string>& optionData)
+{
+    if (optionName.empty())
+    {
+        assert(optionData.empty());
+        return;
+    }
+
+    if (!optionData.empty())
+        options[optionName] = join(optionData, ' ');
+    else
+        options[optionName] = "true";
+
+    optionName.clear();
+    optionData.clear();
+}
+
+OptionMap parseArgs(int argc, const char* argv[])
+{
+    OptionMap options;
+    std::string optionName;
+    std::vector<std::string> optionData;
+
+    for (std::size_t index = 1; index < argc; ++index) // We start at one as argv[0] is binary name
+    {
+        auto currentOption = std::string(argv[index]);
+
+        if (currentOption.front() == '-')
+        {
+            flush(options, optionName, optionData);
+            optionName = currentOption.substr(1);
+        }
+        else if (currentOption.front() == '+')
+        {
+            flush(options, optionName, optionData);
+            options[currentOption.substr(1)] = "false";
+        }
+        else
+        {
+            optionData.push_back(currentOption);
+        }
+    }
+
+    flush(options, optionName, optionData);
+    return options;
+}
